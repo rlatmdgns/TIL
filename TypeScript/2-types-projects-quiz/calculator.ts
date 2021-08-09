87c1cad6322725e426f609e62f81d90c892b1e30
김승훈 <rlatmdgns94@naver.com>
2021-08-09 23:11:17 +0900

    game.ts

--- TypeScript/2-types-projects-quiz/calculator.ts
+++ TypeScript/2-types-projects-quiz/calculator.ts
@@ -2,17 +2,17 @@
  * Let's make a calculator 🧮
  */
 console.log(calculate("add", 1, 3)); // 4
-console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("subtract", 3, 1)); // 2
 console.log(calculate("multiply", 4, 2)); // 8
 console.log(calculate("divide", 4, 2)); // 2
 console.log(calculate("remainder", 5, 2)); // 1
 
-type Command = "add" | "substract" | "multiply" | "divide" | "remainder";
+type Command = "add" | "subtract" | "multiply" | "divide" | "remainder";
 function calculate(command: Command, a: number, b: number): number {
   switch (command) {
     case "add":
       return a + b;
-    case "substract":
+    case "subtract":
       return a - b;
     case "multiply":
       return a * b;
--- TypeScript/2-types-projects-quiz/game.ts
+++ TypeScript/2-types-projects-quiz/game.ts
@@ -1,12 +1,35 @@
 /**
  * Let's make a game 🕹
  */
+let position = { x: 0, y: 0 };
+
+type Direction = "up" | "down" | "left" | "right";
+
+function move(direction: Direction) {
+  switch (direction) {
+    case "up":
+      position.y += 1;
+      break;
+    case "down":
+      position.y -= 1;
+      break;
+    case "left":
+      position.x -= 1;
+      break;
+    case "right":
+      position.x += 1;
+      break;
+    default:
+      break;
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
