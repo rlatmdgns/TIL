a15db43b9c1817209e5482e594b321b67ee305e3
김승훈 <rlatmdgns94@naver.com>
2021-08-10 23:27:14 +0900

    ts -quiz loding

--- TypeScript/2-types-projects-quiz/loading.ts
+++ TypeScript/2-types-projects-quiz/loading.ts
@@ -3,24 +3,40 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
+
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case "loading":
+        console.log("👀 loading...");
+        break;
+      case "success":
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case "fail":
+        console.log(`😃 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state :${state}`);
+    }
+  }
 }
