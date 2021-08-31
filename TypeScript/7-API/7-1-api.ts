6d25ae966d23f4d156876a344af2bdd473dc1fa5
김승훈 <rlatmdgns94@naver.com>
2021-09-01 02:33:12 +0900

    변경

--- TypeScript/7-API/7-1-api.ts
+++ TypeScript/7-API/7-1-api.ts
@@ -16,3 +16,17 @@ const result = students.every((student) => {
 });
 
 console.log(result);
+
+class Animal {}
+class Cat extends Animal {
+  isCat: boolean = true;
+}
+class Dog extends Animal {
+  isDog: boolean = false;
+}
+
+const animals: Animal[] = [new Cat(), new Cat(), new Dog()];
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isCat !== undefined;
+}
+animals.every<Cat>(isCat);
