408fc84c3d949ba525fd5183f0fa129c56eed157
김승훈 <rlatmdgns94@naver.com>
2021-09-05 21:15:06 +0900

    ts: interface

---
+++ TypeScript/9-advanced/9-1-interface.ts
@@ -0,0 +1,55 @@
+{
+  type PositionType = {
+    x: number;
+    y: number;
+  };
+  interface PositionInterface {
+    x: number;
+    y: number;
+  }
+
+  // object ★
+  const obj1: PositionType = {
+    x: 1,
+    y: 1,
+  };
+  const obj2: PositionInterface = {
+    x: 1,
+    y: 1,
+    z: 1,
+  };
+
+  // class ★
+  class Pos1 implements PositionType {
+    x: number;
+    y: number;
+  }
+  class Pos2 implements PositionInterface {
+    x: number;
+    y: number;
+  }
+
+  // Extends
+  interface ZPositionInterface extends PositionInterface {
+    z: number;
+  }
+  type ZPositionType = PositionType & { z: number };
+
+  // 😆 only interfaces can be merged.
+  interface PositionInterface {
+    z: number;
+  }
+
+  // type PositionType {
+  // }
+
+  // 😆 Type aliases can use computed properties
+  type Person = {
+    name: string;
+    age: number;
+  };
+  type Name = Person['name']; // string
+
+  type NumberType = number;
+  type Direction = 'left' | 'right';
+}
