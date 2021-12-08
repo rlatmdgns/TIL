03d6cfb5c85b9a23666b2ea520a5ea0ed71b7651
김승훈 <rlatmdgns94@naver.com>
2021-12-09 03:25:07 +0900

    다형성 버전으로 만들기

--- Javascript/Refactoring2/ch1/createStatementData.js
+++ Javascript/Refactoring2/ch1/createStatementData.js
@@ -1,63 +1,70 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+  get amountFor() {
+    throw new Error("알수 없는 장르");
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amountFor() {
+    let result = 40000;
+    if (this.performance.audience > 30)
+      result += 1000 * (this.performance.audience - 30);
+    return result;
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {
+  get amountFor() {
+    let result = 30000;
+    if (this.performance.audience > 20)
+      result += 10000 + 500 * (this.performance.audience - 20);
+    result += 300 * this.performance.audience;
+    return result;
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
 const createStatementData = (invoice, plays) => {
   const playFor = (aPerformance) => {
     return plays[aPerformance.playID];
   };
 
-  const amountFor = (aPerformance) => {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy": {
-        result = 40000;
-        if (aPerformance.audience > 30)
-          result += 1000 * (aPerformance.audience - 30);
-        break;
-      }
-      case "comedy": {
-        result = 30000;
-        if (aPerformance.audience > 20)
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        result += 300 * aPerformance.audience;
-        break;
-      }
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  };
-
   const totalAmount = (data) => {
-    // let result = 0;
-    // for (let perf of data.performances) {
-    //   result += perf.amount;
-    // }
-    // return result;
     return data.performances.reduce((total, p) => total + p.amount, 0);
   };
 
-  const volumeCreditsFor = (aPerformance) => {
-    let result = 0;
-
-    result += Math.max(aPerformance.audience - 30, 0);
-    if (aPerformance.play.type === "comedy")
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  };
-
   const totalVolumeCredits = (data) => {
-    // let result = 0;
-    // for (let perf of data.performances) {
-    //   result += perf.volumeCredits;
-    // }
-    // return result;
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   };
 
+  const createPerformanceCalculator = (aPerformance, aPlay) => {
+    switch (aPlay.type) {
+      case "tragedy":
+        return new TragedyCalculator(aPerformance, aPlay);
+      case "comedy":
+        return new ComedyCalculator(aPerformance, aPlay);
+      default:
+        return new PerformanceCalculator(aPerformance, aPlay);
+    }
+  };
+
   const enrichPerformance = (aPerformance) => {
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   };
 
--- Javascript/Refactoring2/ch1/main.js
+++ Javascript/Refactoring2/ch1/main.js
@@ -1,10 +1,9 @@
 import { readJSON } from "../fileController.js";
-// import {statement} from "./statement-after.js";
-import {htmlStatement} from "./statement-after.js";
+import {statement} from "./statement-after.js";
 
 const invoices = readJSON("invoices.json");
 const plays = readJSON("plays.json");
 
 invoices.forEach((invoice) => {
-  console.log(htmlStatement(invoice, plays));
+  console.log(statement(invoice, plays));
 });
