32e04dac9b78e57bc0537c0c6ebc6e8d3618c78a
김승훈 <rlatmdgns94@naver.com>
2022-01-06 01:47:17 +0900

    ch7 - 7.4 임시 변수를 질의 함수로 바꾸기

---
+++ Javascript/Refactoring2/ch7/04.js
@@ -0,0 +1,23 @@
+class Order {
+  #quantity;
+  #item;
+  constructor(quantity, item) {
+    this.#quantity = quantity;
+    this.#item = item;
+  }
+  get price() {
+    return this.basePrice * this.discountFactor;
+  }
+
+  get discountFactor() {
+    let discountFactor = 0.98;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return discountFactor;
+  }
+
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+}
+const order = new Order(10, { price: 1000 });
+console.log(order.price);
