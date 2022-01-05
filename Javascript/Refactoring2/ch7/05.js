94e0fbe3f38c51bace8937040bf628a64525f0f2
김승훈 <rlatmdgns94@naver.com>
2022-01-06 01:55:39 +0900

    ch7 - 7.5 클래스 추출하기

---
+++ Javascript/Refactoring2/ch7/05.js
@@ -0,0 +1,37 @@
+class Person {
+  #telephoneNumber;
+  #name;
+  constructor() {
+    this.#telephoneNumber = new TelephoneNumber();
+  }
+  get name() {
+    return this.#name;
+  }
+  set name(arg) {
+    this.#name = arg;
+  }
+  get telephoneNumber() {
+    return this.#telephoneNumber.toString();
+  }
+}
+
+class TelephoneNumber {
+  #areaCode;
+  #number;
+  constructor() {}
+  get areaCode() {
+    return this.#areaCode;
+  }
+  set areaCode(arg) {
+    this.#areaCode = arg;
+  }
+  get number() {
+    return this.#number;
+  }
+  set number(arg) {
+    this.#number = arg;
+  }
+  toString() {
+    return `(${this.#areaCode}) ${this.#number}`;
+  }
+}
