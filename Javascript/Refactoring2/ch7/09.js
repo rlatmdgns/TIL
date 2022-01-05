617cf9ee3b9fa9da09176e2975c377e681f6698d
김승훈 <rlatmdgns94@naver.com>
2022-01-06 02:57:17 +0900

    ch7 - 위임 숨기기, 중개자 제거 , 알고리즘 교체

---
+++ Javascript/Refactoring2/ch7/09.js
@@ -0,0 +1,7 @@
+const findPerson = (people) => {
+  const candidates = ["Don", "John", "Kent"];
+  return people.find((p) => candidates.includes(p)) || "";
+};
+console.log(
+  findPerson(["Roy", "Jay", "Don", "Kay", "John", "Peter", "Kent", "Clark"])
+);
