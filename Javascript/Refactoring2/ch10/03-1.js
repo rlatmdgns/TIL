dc9afa99dfffe1409d773d4312f00bb7d678a4de
김승훈 <rlatmdgns94@naver.com>
2022-02-21 23:35:53 +0900

    중첩 조건문을 보호 구문으로 바꾸기

--- Javascript/Refactoring2/ch10/03-1.js
+++ Javascript/Refactoring2/ch10/03-1.js
@@ -1,14 +1,7 @@
-const payAmount = employee => {
-  let result
-  if (employee.isSeperated) result = { amount: 0, reasonCode: 'SEP' }
+const payAmount = (employee) => {
+  let result;
+  if (employee.isSeperated) return { amount: 0, reasonCode: "SEP" };
   // 퇴사
-  else {
-    if (employee.isRetired) result = { amount: 0, reasonCode: 'RET' }
-    // 은퇴
-    else {
-      // 급여계산
-      result = { amount: 100, reasonCode: 'WRK' } // 재직
-    }
-  }
-  return result
-}
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+  return { amount: 100, reasonCode: "WRK" }; // 재직
+};
--- Javascript/Refactoring2/ch10/03-2.js
+++ Javascript/Refactoring2/ch10/03-2.js
@@ -1,9 +1,7 @@
-const adjustCapital = anInstrument => {
-  let result = 0
-  if (anInstrument.capital > 0) {
-    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-      result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor
-    }
-  }
-  return result
-}
+const adjustCapital = (anInstrument) =>
+  anInstrument.capital <= 0 ||
+  anInstrument.interestRate <= 0 ||
+  anInstrument.duration <= 0
+    ? 0
+    : (anInstrument.income / anInstrument.duration) *
+      anInstrument.adjustmentFactor;
