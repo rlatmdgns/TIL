5ca55a3c5ae48f9314aadc1729273e59cf31fe20
김승훈 <rlatmdgns94@naver.com>
2022-03-01 00:00:16 +0900

    ch11- 세터제거, 생성자 -> 팩토리함수

--- Javascript/Refactoring2/ch11/07.js
+++ Javascript/Refactoring2/ch11/07.js
@@ -1,19 +1,18 @@
 class Person {
-  #name
-  #id
+  #name;
+  #id;
+  constructor(id) {
+    this.#id = id;
+  }
   get name() {
-    return this.#name
+    return this.#name;
   }
   set name(name) {
-    this.#name = name
+    this.#name = name;
   }
   get id() {
-    return this.#id
-  }
-  set id(id) {
-    this.#id = id
+    return this.#id;
   }
 }
-const martin = new Person()
-martin.name = 'Martin'
-martin.id = '1234'
+const martin = new Person("1234");
+martin.name = "Martin";
